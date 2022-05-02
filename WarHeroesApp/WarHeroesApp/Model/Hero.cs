8f5eae0b39de5478c7dab09c9b40f61ee5d2cdbf
Ivan.Ivanov <Ivan.Ivanov@mail.com>
2022-05-03 00:07:21 +0300

    Initial commit

---
+++ WarHeroesApp/WarHeroesApp/Model/Hero.cs
@@ -0,0 +1,11 @@
+﻿namespace WarHeroesApp.Model
+{
+    public class Hero
+    {
+        public string Name { get; init; }
+        public string Surname { get; init; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public Rank Rank { get; init; }
+    }
+}
